377cc0dd83608b0be3fdee3147545a51cb9fd00a
Abdul Said <abdulrahman.asaid@gmail.com>
2022-01-03 21:59:25 -0500

    Finish Exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,11 @@
 class Employee < ActiveRecord::Base
+  validate :first_name
+  validate :last_name
+  validates :hourly_rate, :inclusion => 40..200
+  validate :store_id
+
   belongs_to :store 
+
+  
 
 end
